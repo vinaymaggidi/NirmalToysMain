d1bbb441587466630b348a7e0643b2965f8eb1c3
pettamrakesh3 <122041480+pettamrakesh3@users.noreply.github.com>
2023-06-01 19:07:11 +0530

    Delete src directory

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import index from './index.css';
-
-ReactDOM.render(<App />, document.querySelector('#root'))
