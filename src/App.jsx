1a2dbaaaece801b0d2eb1355d4eab3a6b3bf6996
pettamrakesh3 <pettamrakeshpr@gmail.com>
2023-06-01 19:18:15 +0530

    public

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
 import './index.css';
-import Header from './components/Header/Header';
+import StickyHeader from './components/Header/StickyHeader'
 import Navbar from './components/NavBar/NavBar';
 import About from './components/About/About';
-import Services from './components/Services/Services'
+import Services from './components/Services/Services.jsx';
 import Contact from './components/Contact/Contact';
 import Footer from './components/Footer/Footer'
+
+import ImageSlider from './components/Slides/ImageSlider';
+import { SliderData } from './components/Slides/SliderData';
+
 
 const App = () => {
     return (
         <>
-            <Header />
+            <StickyHeader/>
+            <Navbar/>
+            <ImageSlider slides={SliderData} />
             <About />
             <Services />
             <Contact />
---
+++ src/components/Slides/SliderData.jsx
@@ -0,0 +1,22 @@
+import Slide1 from '../../assets/slide1.jpg'
+import Slide2 from '../../assets/slide2.jpg'
+import Slide3 from '../../assets/slide3.jpg'
+import Slide4 from '../../assets/slide4.jpg'
+import Slide5 from '../../assets/slide5.jpg'
+export const SliderData = [
+    {
+      image:Slide1
+    },
+    {
+        image:Slide2
+    },
+    {
+        image:Slide3
+    },
+    {
+        image:Slide4
+    },
+    {
+        image:Slide5
+    } 
+  ];
